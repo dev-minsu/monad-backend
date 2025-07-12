c0ec1003b4428d6af1a10abae8240e9fb26367df
Kim Min Su <kms860226@naver.com>
2025-07-12 17:18:27 +0900

    fix: change prefix

--- src/monad/interface/controller/monad.controller.ts
+++ src/monad/interface/controller/monad.controller.ts
@@ -1,21 +1,21 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { MonadService } from '../../application/service/monad.service';
 
-@Controller('monad')
+@Controller('api')
 export class MonadController {
   constructor(private readonly monadService: MonadService) {}
 
-  @Post('/user')
+  @Post('monad/user')
   createUser(@Body('address') address: string) {
     return this.monadService.createUser(address);
   }
 
-  @Post('/user/attack')
+  @Post('monad/user/attack')
   attackEnemy(@Body('address') address: string) {
     return this.monadService.attackEnemy(address);
   }
 
-  @Get('/user/:address')
+  @Get('monad/user/:address')
   getUser(@Param('address') address: string) {
     return this.monadService.getUser(address);
   }
